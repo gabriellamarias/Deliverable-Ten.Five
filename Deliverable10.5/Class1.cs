d6577809e1c226c5c7f8d764c362c1640cfdf556
gabi <gabriella.marias@gmail.com>
2021-09-03 12:55:38 -0400

    First commit for deliverable ten point five, (exercise 50)

---
+++ Deliverable10.5/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Deliverable10._5
+{
+    public class Triangle
+    {
+        public int sideLengthOne { get; set; }
+        public int sideLengthTwo { get; set;}
+        public int sideLengthThree { get; set; }
+        public Triangle (int sideLengthOne, int sideLengthTwo, int sideLengthThree)
+        {
+            this.sideLengthOne = sideLengthOne;
+            this.sideLengthTwo = sideLengthTwo;
+            this.sideLengthThree = sideLengthThree;
+
+        }
+
+       
+   
+
+    }
+}
---
+++ Deliverable10.5/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Deliverable10._5
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<Triangle> userTriangles = new List<Triangle>();
+            bool userContinue = true;
+
+            while (userContinue == true)
+            {
+                Console.Write("Enter the side lengths of a triangle (q to quit): ");
+
+                var userInput = Console.ReadLine();
+
+                if (userInput != "q")
+                {
+                    var sides = userInput.Split(' ');
+
+                    int usersideLengthOne = int.Parse(sides[0]);
+                    int usersideLengthTwo = int.Parse(sides[1]);
+                    int usersideLengthThree = int.Parse(sides[2]);
+
+
+                    Triangle t = new Triangle(usersideLengthOne, usersideLengthTwo, usersideLengthThree);
+
+                    userTriangles.Add(t);
+                }
+                else if (userInput == "q")
+                {
+
+                    Console.WriteLine($"Average Area: {Area(userTriangles)}");
+                    Console.WriteLine($"Average Perimeter: {Perimeter(userTriangles)}");
+
+                    Console.Write("Do you wish to continue (y/n)? ");
+                    userInput = Console.ReadLine();
+                    userTriangles.Clear();
+
+                    if (userInput == "n")
+                    {
+                        userContinue = false;
+
+                        Console.WriteLine("Goodbye!");
+                    }
+                }
+            }
+        }
+
+        public static double Perimeter(List<Triangle> userTriangles)
+        {
+            List<int> addedTriangles = new List<int>();
+            foreach (Triangle c in userTriangles)
+            {
+                int x = c.sideLengthOne + c.sideLengthTwo + c.sideLengthThree;
+                addedTriangles.Add(x);
+            }
+            return addedTriangles.Average();
+            
+        }
+        public static double Area(List<Triangle> userTriangles)
+        {
+            List<int> areaTriangles = new List<int>();
+            foreach (Triangle c in userTriangles)
+            {
+                int x = (c.sideLengthOne * c.sideLengthTwo) / 2;
+                areaTriangles.Add(x);
+            }
+            return areaTriangles.Average();
+        }
+    }
+}
